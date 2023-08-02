57177a3551b3e21fedafb0d86d2775e16615cc48
MaxAnii <ansarbaba2000@gmail.com>
2023-08-02 19:41:22 +0530

    removed the remove button from the successful sign

--- client/src/components/SignupSuccess.jsx
+++ client/src/components/SignupSuccess.jsx
@@ -3,16 +3,10 @@ import React from "react";
 const SignupSuccess = () => {
   return (
     <div
-      className="alert alert-warning alert-dismissible fade show"
+      className="alert alert-success alert-dismissible fade show"
       role="alert"
     >
       <strong>Signup Successful !</strong> Please login to access your profile.
-      <button
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-      ></button>
     </div>
   );
 };
