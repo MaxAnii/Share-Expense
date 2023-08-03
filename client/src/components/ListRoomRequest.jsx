83438405632b05b87804c32879ae23b2a116041e
MaxAnii <ansarbaba2000@gmail.com>
2023-08-03 22:22:51 +0530

    component to get and display room requset list

---
+++ client/src/components/ListRoomRequest.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import AcceptRequest from "./AcceptRequest";
+import RejectRequest from "./RejectRequest";
+
+const ListRoomRequest = (props) => {
+  console.log(props);
+  const color = ["black", "dimgray", "darkgray", "silver", "gray"];
+  var colorIndex = 0;
+  const index = () => {
+    colorIndex++;
+    if (colorIndex >= color.length) colorIndex = 0;
+    return colorIndex;
+  };
+  const [requestList, setRequestList] = useState([]);
+  const [message, setMessage] = useState("");
+  const getRequestList = async () => {
+    setMessage("");
+    const response = await fetch(
+      `http://localhost:5000/user/roomrequest/${props.userid}`,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "Application/json",
+          "Content-Type": "Application/json",
+        },
+      }
+    );
+    const data = await response.json();
+
+    if (data.status === 400) {
+      setMessage("No new request");
+    } else {
+      setRequestList(data);
+    }
+  };
+  useEffect(() => {
+    getRequestList();
+  }, []);
+
+  return (
+    <>
+      <div className="room-table">
+        <table className="table table-light  table-hover">
+          <tbody>
+            {requestList.map((elem) => {
+              return (
+                <>
+                  <tr className="table-row room-row">
+                    <td>
+                      <button
+                        className="room-image"
+                        style={{ backgroundColor: color[index()] }}
+                      >
+                        {elem.name[0].toUpperCase()}
+                      </button>
+                    </td>
+                    <td>
+                      <p className="room-name">{elem.name.toUpperCase()}</p>
+                      <p className="room-description">{elem.description}</p>
+                    </td>
+                    <td>
+                      <AcceptRequest
+                        userid={elem.memberid}
+                        roomid={elem.roomid}
+                      ></AcceptRequest>
+                    </td>
+                    <td>
+                      <RejectRequest
+                        userid={elem.memberid}
+                        roomid={elem.roomid}
+                      ></RejectRequest>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default ListRoomRequest;
