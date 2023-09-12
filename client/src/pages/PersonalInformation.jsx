ca43dee7e260207c0df6324e772c233422f72e38
MaxAnii <ansarbaba2000@gmail.com>
2023-09-12 23:00:39 +0530

    Added an readonly input tag to show user details

--- client/src/pages/PersonalInformation.jsx
+++ client/src/pages/PersonalInformation.jsx
@@ -1,7 +1,59 @@
 import React from "react";
-
-const PersonalInformation = () => {
-  return <div className="form-container"></div>;
+import Google from "../img/google.png";
+import Github from "../img/github.png";
+import ChangePassword from "../components/ChangePassword";
+const PersonalInformation = ({ user }) => {
+  return (
+    <div className="form-container">
+      <div className="basic-information">
+        <div className="user-avatar-container">
+          <img src={user.image} alt="" className="user-avatar" />
+        </div>
+        <div className="user-name-container">
+          <div className="user-name">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+              User Name
+            </label>
+            <input
+              type="text"
+              value={user.name}
+              className="form-control"
+              readOnly
+            />
+          </div>
+          <div className="user-bio">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+              Bio
+            </label>
+            <input type="text" placeholder="xyz" className="form-control" />
+          </div>
+        </div>
+      </div>
+      <div className="other-details">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Image Url
+        </label>
+        <input
+          type="text"
+          value={user.image}
+          className="form-control"
+          readOnly
+        />
+      </div>
+      <div className="other-details">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Email
+        </label>
+        <input
+          type="text"
+          value={user.email}
+          className="form-control"
+          readOnly
+        />
+      </div>
+      <ChangePassword></ChangePassword>
+    </div>
+  );
 };
 
 export default PersonalInformation;
