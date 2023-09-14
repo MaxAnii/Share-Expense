49a784712b9d7abdfa9dc79285666693b200fdbf
MaxAnii <ansarbaba2000@gmail.com>
2023-09-14 22:25:25 +0530

    Sending user details to end the backend.

--- client/src/pages/PersonalInformation.jsx
+++ client/src/pages/PersonalInformation.jsx
@@ -1,73 +1,142 @@
-import React from "react";
-import Google from "../img/google.png";
-import Github from "../img/github.png";
+import React, { useState } from "react";
 import ChangePassword from "../components/ChangePassword";
 const PersonalInformation = ({ user }) => {
+  const [userDetails, setUserDetails] = useState({
+    ...user,
+  });
+  const [message, setMessage] = useState("");
+  const [readOnly, setReadOnly] = useState(true);
+  const updateDetails = async () => {
+    const response = await fetch("http://localhost:5000/user/updatedetails", {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        Accept: "Application/json",
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(userDetails),
+    });
+    const data = await response.json();
+    setMessage(data.message);
+  };
   return (
-    <div className="form-container">
-      <div className="basic-information">
-        <div className="user-avatar-container">
-          <img src={user.image} alt="" className="user-avatar" />
-        </div>
-        <div className="user-name-container">
-          <div className="user-name">
-            <label htmlFor="exampleFormControlInput1" className="form-label">
-              User Name
-            </label>
-            <input
-              type="text"
-              value={user.name}
-              className="form-control form-control-lg"
-              readOnly
+    <>
+      <div className="form-container">
+        {readOnly ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-pencil btn btn-dark edit-details"
+            viewBox="0 0 16 16"
+            onClick={() => {
+              setReadOnly(false);
+            }}
+          >
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-check2-all btn btn-success edit-details"
+            viewBox="0 0 16 16"
+            onClick={() => {
+              updateDetails();
+              setReadOnly(true);
+            }}
+          >
+            <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z" />
+            <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z" />
+          </svg>
+        )}
+
+        <div className="basic-information">
+          <div className="user-avatar-container">
+            <img
+              src={userDetails.image}
+              alt="user profile"
+              className="user-avatar"
             />
           </div>
-          <div className="user-bio">
-            <label htmlFor="exampleFormControlInput1" className="form-label">
-              Bio
-            </label>
-            <input
-              type="text"
-              placeholder="xyz"
-              className="form-control form-control-lg"
-            />
+          <div className="user-name-container">
+            <div className="user-name">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                User Name
+              </label>
+              <input
+                type="text"
+                value={userDetails.name}
+                className="form-control form-control-lg"
+                readOnly={readOnly}
+                onChange={(e) => {
+                  setUserDetails({ ...userDetails, name: e.target.value });
+                }}
+              />
+            </div>
+            <div className="user-bio">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Bio
+              </label>
+              <textarea
+                className="form-control  form-control-lg"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                cols="60"
+                readOnly={readOnly}
+                value={userDetails.bio}
+                onChange={(e) => {
+                  setUserDetails({ ...userDetails, bio: e.target.value });
+                }}
+              ></textarea>
+            </div>
           </div>
         </div>
+        <div className="other-details">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Image Url
+          </label>
+          <input
+            type="text"
+            value={userDetails.image}
+            className="form-control form-control-lg"
+            readOnly={readOnly}
+            onChange={(e) => {
+              setUserDetails({ ...userDetails, image: e.target.value });
+            }}
+          />
+        </div>
+        {user.editFlag === "editable" ? (
+          <>
+            <div className="other-details">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Email
+              </label>
+              <input
+                type="text"
+                value={userDetails.email}
+                className="form-control form-control-lg"
+                readOnly={readOnly}
+                onChange={(e) => {
+                  setUserDetails({ ...userDetails, email: e.target.value });
+                }}
+              />
+            </div>
+            <ChangePassword userId={user.id}></ChangePassword>
+          </>
+        ) : (
+          <div className="inforamtion">
+            <div class="alert alert-dark" role="alert">
+              You can't change Password or email because you have signed up
+              using <b className="edit-flag">{user.editFlag}</b>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="other-details">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          Image Url
-        </label>
-        <input
-          type="text"
-          value={user.image}
-          className="form-control form-control-lg"
-          readOnly
-        />
-      </div>
-      {user.editFlag === "editable" ? (
-        <>
-          <div className="other-details">
-            <label htmlFor="exampleFormControlInput1" className="form-label">
-              Email
-            </label>
-            <input
-              type="text"
-              value={user.email}
-              className="form-control"
-              readOnly
-            />
-          </div>
-          <ChangePassword userId={user.id}></ChangePassword>
-        </>
-      ) : (
-        <div className="inforamtion">
-          <div class="alert alert-dark" role="alert">
-            You can't change Password or email because you have signed up using{" "}
-            {user.editFlag}
-          </div>
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
