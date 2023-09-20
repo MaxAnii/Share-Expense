cdcba9ae4abdc05ea3022a9669ad9ac2077a20b8
MaxAnii <ansarbaba2000@gmail.com>
2023-09-20 21:11:48 +0530

    changed class to className.

--- client/src/components/RejectRequest.jsx
+++ client/src/components/RejectRequest.jsx
@@ -30,7 +30,7 @@ const RejectRequest = (props) => {
         width="39"
         height="100"
         fill="currentColor"
-        class="bi bi-x-square"
+        className="bi bi-x-square"
         viewBox="0 0 16 16"
       >
         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
