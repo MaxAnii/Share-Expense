7463b9318a698006f7c3160a0723653c8b5e2d45
MaxAnii <ansarbaba2000@gmail.com>
2023-09-20 21:02:57 +0530

    changed fill-rule to fillRule changed class to className

--- client/src/components/AddMember.jsx
+++ client/src/components/AddMember.jsx
@@ -60,7 +60,7 @@ const AddMember = (props) => {
       >
         <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
         />
       </svg>
@@ -110,7 +110,7 @@ const AddMember = (props) => {
                   />
                   <button
                     type="submit"
-                    class="input-group-text"
+                    className="input-group-text"
                     id="basic-addon1"
                   >
                     <svg
@@ -118,7 +118,7 @@ const AddMember = (props) => {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="bi bi-search"
+                      className="bi bi-search"
                       viewBox="0 0 16 16"
                     >
                       <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
@@ -129,16 +129,17 @@ const AddMember = (props) => {
 
               <p className="error-message"> {message}</p>
               <div className=".overflow-member-table">
-                <table class="table">
+                <table className="table">
                   <thead>
                     <tr></tr>
                   </thead>
                   <tbody>
-                    {userDetails.map((elem) => {
+                    {userDetails.map((elem, i) => {
                       return (
-                        <>
-                          {elem.id !== props.userid ? ( //stopping user to send request to himself
-                            <tr>
+                        <tr key={i} className="row-gap">
+                          {elem.id !== props.userid ? (
+                            //stopping user to send request to himself
+                            <>
                               <td className="user-info">
                                 <img
                                   src={elem.image}
@@ -162,23 +163,22 @@ const AddMember = (props) => {
                                     width="30"
                                     height="65"
                                     fill="currentColor"
-                                    class="bi bi-person-plus-fill"
+                                    className="bi bi-person-plus-fill"
                                     viewBox="0 0 16 16"
                                   >
                                     <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
                                     <path
-                                      fill-rule="evenodd"
+                                      fillRule="evenodd"
                                       d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
                                     />
                                   </svg>
                                 </button>
                               </td>
-                            </tr>
+                            </>
                           ) : (
                             ""
                           )}
-                          <div className="row-gap"></div>
-                        </>
+                        </tr>
                       );
                     })}
                   </tbody>
