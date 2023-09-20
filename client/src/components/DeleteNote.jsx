02e275378666800fc7b6586300a89f55cd97238d
MaxAnii <ansarbaba2000@gmail.com>
2023-09-20 21:05:28 +0530

    changed class to className. changed tabindex to tabIndex.

--- client/src/components/DeleteNote.jsx
+++ client/src/components/DeleteNote.jsx
@@ -36,7 +36,7 @@ const DeleteNote = (props) => {
             width="25"
             height="40"
             fill="currentColor"
-            class="bi bi-trash"
+            className="bi bi-trash"
             viewBox="0 0 16 16"
           >
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
@@ -45,41 +45,41 @@ const DeleteNote = (props) => {
         </button>
 
         <div
-          class="modal fade"
+          className="modal fade"
           id="delete-Note"
           data-bs-backdrop="static"
           data-bs-keyboard="false"
-          tabindex="-1"
+          tabIndex="-1"
           aria-labelledby="staticBackdropLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">
                   Warning
                 </h1>
                 <button
                   type="button"
-                  class="btn-close modal-close-btn"
+                  className="btn-close modal-close-btn"
                   data-bs-dismiss="modal"
                   aria-label="Close"
                 ></button>
               </div>
-              <div class="modal-body">
+              <div className="modal-body">
                 <h2>All the note data will be deleted.</h2>
               </div>
-              <div class="modal-footer">
+              <div className="modal-footer">
                 <button
                   type="button"
-                  class="btn btn-secondary"
+                  className="btn btn-secondary"
                   data-bs-dismiss="modal"
                 >
                   Close
                 </button>
                 <button
                   type="button"
-                  class="btn btn-danger"
+                  className="btn btn-danger"
                   data-bs-dismiss="modal"
                   onClick={deleteNote}
                 >
