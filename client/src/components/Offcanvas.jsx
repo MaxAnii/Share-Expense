d5995c8bc8ff5f5d11809cf3dcacb2cdfcb63817
MaxAnii <ansarbaba2000@gmail.com>
2023-09-20 21:11:00 +0530

    changed fill-rule to fillRule. changed tabindex to tabIndex.

--- client/src/components/Offcanvas.jsx
+++ client/src/components/Offcanvas.jsx
@@ -24,7 +24,7 @@ const Offcanvas = () => {
           viewBox="0 0 16 16"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
           />
         </svg>
@@ -32,7 +32,7 @@ const Offcanvas = () => {
 
       <div
         className="offcanvas offcanvas-start"
-        tabindex="-1"
+        tabIndex="-1"
         id="offcanvasExample"
         aria-labelledby="offcanvasExampleLabel"
       >
