0cdfa12299d438dbb3c27c0a2db5b4a73873685d
MaxAnii <ansarbaba2000@gmail.com>
2023-09-25 23:47:13 +0530

    Adding cookie domain

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 15 * 24 * 60 * 60 * 1000, // 1 day in milliseconds
-      domain: "localhost:3000",
+      domain: "http://localhost:3000",
     },
   })
 );
