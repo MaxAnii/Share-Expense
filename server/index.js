7c1ef16bbc2e22d49264584c7e438aab23f8ab36
MaxAnii <ansarbaba2000@gmail.com>
2023-09-26 06:38:47 +0530

    Adding initial setting for cookies.

--- server/index.js
+++ server/index.js
@@ -30,13 +30,13 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.get("/check", check);
 app.use("/auth", authRoute);
