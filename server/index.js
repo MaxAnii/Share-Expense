e43278ea068a3ff6cbafa824818e2e69e41c4a10
MaxAnii <ansarbaba2000@gmail.com>
2023-09-26 10:05:34 +0530

    universal origin

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 app.use(passport.authenticate("session"));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
